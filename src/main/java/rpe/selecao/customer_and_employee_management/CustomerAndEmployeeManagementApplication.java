1852e56f80ed1d0987c9fdf412b783a53a563e35
wagnerdgarcia <wagnergarcia@cc.ci.ufpb.br>
2023-01-11 15:13:52 -0300

    Init Commit

---
+++ src/main/java/rpe/selecao/customer_and_employee_management/CustomerAndEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package rpe.selecao.customer_and_employee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAndEmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAndEmployeeManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rpe/selecao/customer_and_employee_management/CustomerAndEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package rpe.selecao.customer_and_employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAndEmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
